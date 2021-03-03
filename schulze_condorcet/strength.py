e82bdac44ca655d3481e98379cfeabdc420bd36e
Lars Esser <mail@lars-esser.de>
2021-03-03 14:00:23 +0100

    schulze-condorcet: move definition of strength functions in own file

---
+++ schulze_condorcet/strength.py
@@ -0,0 +1,49 @@
+""" Example implementations to define the strength of a link in the Schulze problem.
+
+We view the candidates as vertices of a graph and determining the result as the
+strongest path in the graph. To determine the strength of a path, we have to define a
+metric which takes the voting of the voters into account.
+
+Therefore, we transform every vote string into a number of supports and a number of
+oppositions per path in the graph of candidates (for details, see at `schulze_evaluate`.
+Now, we use this and the total number of votes to define the strength of a given path
+in the graph of all candidates.
+
+How to asses the strength of a path is one thing not specified by the Schulze method and
+indeed there are several possibilities. We offer some sample implementations of such a
+strength function, together with an abstract function which can be used as a blue print
+to implement your own strength function.
+"""
+
+
+def strength_example(support: int, opposition: int, totalvotes: int) -> int:
+    """A generic example of a strength function."""
+    raise NotImplementedError
+
+
+def winning_votes(support: int, opposition: int, totalvotes: int) -> int:
+    """This strategy is also advised by the paper of Markus Schulze.
+
+    If two two links have more support than opposition, then the link with more
+    supporters is stronger, if supporters tie then less opposition is used as secondary
+    criterion.
+    """
+    if support > opposition:
+        return totalvotes * support - opposition
+    elif support == opposition:
+        return 0
+    else:
+        return -1
+
+
+def margin(support: int, opposition: int, totalvotes: int) -> int:
+    """This strategy which seems to have a more intuitive appeal.
+
+    It sets the difference between support and opposition as strength of a link. However
+    the discrepancy between this strategy and `winning_votes` is rather small, to wit
+    all cases in the test suite give the same result for both of them.
+
+    Moreover if the votes contain no ties both strategies (and several more) are totally
+    equivalent.
+    """
+    return support - opposition
